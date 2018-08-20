6325b6cc9d2a15586e6b427939d1dab73fb51b2f
Mateusz Kocz <mateusz@kocz.pl>
2018-08-20 20:09:00 +0200

    Add elements JSON

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,30 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+defmodule Properties do
+  defp get_props(props, []) do
+    props
+  end
+
+  defp get_props(props, [element | list]) do
+    new_props =
+      element
+      |> Map.keys()
+      |> MapSet.new()
+      |> MapSet.union(props)
+    get_props(new_props, list)
+  end
+
+  def get_properties(list) do
+    get_props(MapSet.new(), list)
+  end
+end
+
+path = Path.expand("priv/repo/elements.json")
+{:ok, body} = File.read(path)
+{:ok, %{"elements" => elements}} = Poison.decode(body)
+
+props = Properties.get_properties(elements)
+
+IO.inspect MapSet.to_list(props)
